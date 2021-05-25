c5bba29cd0193aa9d112c8057cc1a4b9c2befac2
Sushil S Garde <sushilgarde@gmail.com>
2021-05-25 19:40:34 +0530

    background music fix

--- client/src/components/Cards.vue
+++ client/src/components/Cards.vue
@@ -1,12 +1,12 @@
 <template>
   <div id='card-layout' style='margin: 2rem;'>
+    <!-- img-src="https://www.msgflower.com/birthday/images/birthday-messages-image.jpg" -->
     <b-card
       :title="'Greetings from '+greeting.userName"
-      img-src="https://www.msgflower.com/birthday/images/birthday-messages-image.jpg"
       img-alt="Image"
       img-top
       tag="article"
-      style="max-width: 30rem; margin: 2rem 0"
+      style="max-width: 30rem; margin: 2rem 0;background:linear-gradient(30deg, rgb(44, 188, 99), rgb(22, 164, 138) 70%);color:white"
       class="mb-2"
       v-for='greeting in greetings'
       :key='greeting'
--- client/src/components/Timer.vue
+++ client/src/components/Timer.vue
@@ -20,59 +20,8 @@
 </template>
 
 <script>
-import $ from 'jquery'
 export default {
-  data(){
-    return{
-       player: new Audio(),
-    }
-  },
-  mounted() {
-    var _this = this;
-    const daysEl = document.getElementById("days");
-    const hoursEl = document.getElementById("hours");
-    const minsEl = document.getElementById("mins");
-    const secondsEl = document.getElementById("seconds");
-
-    const newYears = "30 May 2021";
-
-    function countdown() {
-      const newYearDate = new Date(newYears);
-      const currentDate = new Date();
-
-      const totalSeconds = (newYearDate - currentDate) / 1000;
-
-      const days = Math.floor(totalSeconds / 3600 / 24);
-      const hours = Math.floor(totalSeconds / 3600) % 24;
-      const minutes = Math.floor(totalSeconds / 60) % 60;
-      const seconds = Math.floor(totalSeconds) % 60;
-
-      daysEl.innerHTML = days;
-      hoursEl.innerHTML = formatTime(hours);
-      minsEl.innerHTML = formatTime(minutes);
-      secondsEl.innerHTML = formatTime(seconds);
-    }
-
-    function formatTime(time) {
-      return time < 10 ? `0${time}` : time;
-    }
-
-    countdown();
-
-    setInterval(countdown, 1000);
-
-    $(document).ready(function(){ 
-      swal({
-            title: `Time left for Birthday! Days: ${daysEl.innerHTML } Hrs: ${hoursEl.innerHTML } Mins: ${minsEl.innerHTML }`,
-            icon: "info",
-            dangerMode: false,
-        })
-        .then((ok) => {
-          _this.player.src = require('../assets/happybday.mp3');
-          _this.player.play();
-        })
-    });
-  },
+  
 };
 </script>
 
